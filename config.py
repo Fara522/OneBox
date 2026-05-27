6b859824fad2380290bb4a892abfa8641925c6f1
Fara522 <safarboyevfarruxbek56@gmail.com>
2026-05-27 12:00:13 +0500

    fix one-box

--- config.py
+++ config.py
@@ -1,12 +1,28 @@
 import os
-from dotenv import load_dotenv
+import aiomysql
 
-load_dotenv()
+pool = None
 
-BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+async def create_pool():
+    global pool
 
-DB_HOST: str = os.getenv("DB_HOST", "localhost")
-DB_PORT: int = int(os.getenv("DB_PORT", "3306"))
-DB_USER: str = os.getenv("DB_USER", "root")
-DB_PASSWORD: str = os.getenv("DB_PASSWORD", "")
-DB_NAME: str = os.getenv("DB_NAME", "onebox_bot")
+    pool = await aiomysql.create_pool(
+        host=os.getenv("MYSQLHOST"),
+        port=int(os.getenv("MYSQLPORT")),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        db=os.getenv("MYSQLDATABASE"),
+    )
+
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+
+# DB_HOST: str = os.getenv("DB_HOST", "localhost")
+# DB_PORT: int = int(os.getenv("DB_PORT", "3306"))
+# DB_USER: str = os.getenv("DB_USER", "root")
+# DB_PASSWORD: str = os.getenv("DB_PASSWORD", "")
+# DB_NAME: str = os.getenv("DB_NAME", "onebox_bot")
